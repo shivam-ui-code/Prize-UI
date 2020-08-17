df24de7b8894825149221f8e7965f713f70e3fb7
Shivam Gupta <shivam.gupta@flexential.com>
2020-08-17 14:03:46 +0530

    changes regarding react page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import start from './music/Drum Roll - Gaming Sound Effect (HD).mp3';
+import annoucement from './music/Party Horn Sound Effect.mp3'
+import Confetti from 'react-dom-confetti';
+import { winner } from './config/winnerConfig';
 
 function App() {
+  const [hurray, effect] = useState(false);
+  const [winnerDetail, winnerDetails] = useState({
+    placeholder: '',
+    name: '',
+    prize: '',
+    className: ''
+  })
+
+  let count = 0;
+  const toggle = () => {
+    count ? phaseTwo() : phaseOne();
+  }
+
+  const phaseOne = () => {
+    count++;
+    effect(false);
+    new Audio(start).play();
+  }
+
+
+
+  const phaseTwo = () => {
+    console.log(winner());
+    count = 0;
+    new Audio(annoucement).play();
+    setTimeout(() => {
+      winnerDetails({ ...winner() });
+      new Audio(annoucement).play();
+      effect(true);
+    }, 2000)
+
+
+
+  }
+  const config = {
+    angle: 90,
+    spread: 360,
+    startVelocity: 20,
+    elementCount: 200,
+    dragFriction: 0.03,
+    duration: 8000,
+    stagger: 2,
+    width: "20px",
+    height: "15px",
+    perspective: "500px",
+    colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div className="App" onClick={toggle} >
+      <Confetti active={hurray} config={config} />
+      <p class='image'>And the winner is...</p>
+      <div class="container">
+        <Confetti active={hurray} config={config} />
+        <div class="box">
+          <p className={`react-transition ${winnerDetail.className}`}>{winnerDetail.placeholder}</p>
+          <p className={`react-transition ${winnerDetail.className}`}>{winnerDetail.name}</p>
+          <p className={`react-transition ${winnerDetail.className}`}>{winnerDetail.prize}</p>
+          <Confetti active={hurray} config={config} />
+        </div>
+      </div>
+      <div class='img'>
+      </div>
+    </div >
+
   );
 }
 
---
+++ src/config/winnerConfig.js
@@ -0,0 +1,12 @@
+
+
+export const winner =  () => (
+      {
+        placeholder: '1st Prize goes toooooooo',
+        name:'Shivam Gupta',
+        prize: '$100000',
+        className: 'swipe-up'
+    }
+)
+
+
